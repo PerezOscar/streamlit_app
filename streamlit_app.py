721c1627d2bcc1dabe52665d1e219dbe15d73409
PerezOscar <63068152+PerezOscar@users.noreply.github.com>
2023-07-31 20:57:17 -0600

    Update streamlit_app.py
    adds stop streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 #New section to display fruityvice api response
 import requests
-import snowflake.connector
 
 streamlit.title('My parents Mew Healthy Diner')
 
@@ -34,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
@@ -41,5 +42,9 @@ my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('Thanks for adding ', fruit_choice)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#This will work correctly, but just go with it for now
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
+
